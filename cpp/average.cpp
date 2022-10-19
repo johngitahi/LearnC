5411e85b5ed005c7585199406f49d2c2a307b6c9
Gitahi <gitahi109@gmail.com>
2022-10-19 13:57:53 +0300

    C++ basic statements

---
+++ cpp/average.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float number1, number2, sum, average;
+    cout << "Enter two numbers: "; // prompt
+    cin >> number1;
+    cin >> number2;                // Reads numbers from keyboard
+
+    sum = number1 + number2;
+    average = sum/2;
+
+    cout << "Sum = " << sum << "\n";
+    cout << "Average = " << average << "\n";
+
+    return 0;
+}
---
+++ cpp/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // include the header file
+
+using namespace std;
+int main() {
+    cout << "C++ is better than C.\n"; // C++ statement
+
+    return 0;
+}
